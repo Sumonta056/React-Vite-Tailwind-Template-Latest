e63c8409e4c283ecd5af35332330ae8edaafec2a
Sumonta Saha Mridul <61287791+Sumonta056@users.noreply.github.com>
2024-03-13 21:30:10 +0600

    Update website title and add cart functionality

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ export default function App() {
   const [products, setProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(false);
+  const [cart, setCart] = useState([]);
+
+  function onClickButton() {
+    setCart(false);
+  }
 
   const fetchData = async () => {
     setIsLoading(true); //  Go to loading state
@@ -26,7 +31,71 @@ export default function App() {
 
   useEffect(() => {
     fetchData();
+    // setCart(false);
   }, []);
+
+  if (!cart) {
+    return (
+      <div className="flex flex-col items-center justify-center w-full h-screen mx-auto bg-slate-800">
+        <div className="max-w-lg p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+          <a href="#">
+            <h4 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              ! Add to Cart Under Development
+            </h4>
+          </a>
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            This feature is currently under development. Please wait for further
+            updates. We appreciate your patience and understanding.
+          </p>
+          <div className="flex justify-between gap-2">
+            <a
+              href="https://github.com/sumonta056"
+              className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Follow Me
+              <svg
+                className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="1.5"
+                  d="M1 5h12M8 1l4 4-4 4"
+                ></path>
+              </svg>
+            </a>
+
+            <button
+              className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-slate-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-slate-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              onClick={() => setCart(true)}
+            >
+              Go Back
+              <svg
+                className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="1.5"
+                  d="M1 5h12M8 1l4 4-4 4"
+                ></path>
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -46,7 +115,7 @@ export default function App() {
             bordered={true}
             style={{
               width: 330,
-              height: 560,
+              height: 520,
             }}
             cover={
               <img
@@ -68,9 +137,21 @@ export default function App() {
                   {product.title}
                 </span>
               }
-              description={product.description}
+              description={
+                <div
+                  style={{
+                    display: "-webkit-box",
+                    WebkitLineClamp: 1,
+                    WebkitBoxOrient: "vertical",
+                    overflow: "hidden",
+                    textOverflow: "ellipsis",
+                  }}
+                >
+                  {product.description}
+                </div>
+              }
             />
-            <div className="flex flex-col justify-end h-full">
+            <div className="flex flex-col justify-between">
               <div className="mt-2 text-lg">
                 Discount: {product.discountPercentage}%
               </div>
@@ -78,10 +159,12 @@ export default function App() {
               <div className="text-lg">Stock: {product.stock}</div>
               <div className="text-lg">Brand: {product.brand}</div>
               <div className="text-lg">Category: {product.category}</div>
-
-              <div className="flex items-center justify-between bottom-3">
+              <div className="flex items-center justify-between mt-6">
                 <span className="text-2xl font-bold">${product.price}</span>
-                <button className="text-white bg-slate-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                <button
+                  className="text-white bg-slate-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                  onClick={onClickButton}
+                >
                   Add to cart
                 </button>
               </div>
